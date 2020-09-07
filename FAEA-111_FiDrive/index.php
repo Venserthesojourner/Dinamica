319666d2e9dbb629ded224535d4a6527481019c5
Venserthesojourner <luchoc2812@gmail.com>
2020-09-07 10:18:24 -0300

    Este es el proyecto en su etapa inicial

---
+++ FAEA-111_FiDrive/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indice</title>
+</head>
+<body>
+    <div>
+    <h1>Trabajo Practico Entregable</h1>
+    <h2>Parte 0</h2>
+    <hr>
+    <p>Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.</p>
+    <p>Mis nombres es: Ca&ntilde;ete Luis Miguel</p>
+    <p>Nro de Legajo: FAEA-111</p>
+    </div>
+</body>
+</html>
---
+++ TP1/puntosiete_form.php
@@ -0,0 +1,23 @@
+<!-- Ejercicio 7
+Crear una página con un formulario que contenga dos input de tipo text y un select. En
+los inputs se ingresarán números y el select debe dar la opción de una operación
+matemática que podrá resolverse usando los números ingresados. En la página que
+procesa la información se debe mostrar por pantalla la operación seleccionada, cada
+uno de los operandos y el resultado obtenido de resolver la operación. Ejemplo del
+formulario: -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Siete</title>
+</head>
+<body>
+    <form action="" method="get">
+    <input type="text">
+    <input type="text">
+    <select name="operaciones" id="operaciones"></select>
+    </form>
+</body>
+</html>
